7ac3dd2196387067accf660125697fc35815a271
Cindila Sunny <cindilakaranchirakkal@gmail.com>
2024-02-27 01:29:46 +0530

    Inserting new Employee

---
+++ Sample-Dapper/Employees.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_Dapper
+{
+  public class Employees
+    {
+        public int ID { get; set; }
+        public string EmpName { get; set; }
+        public string City{ get; set; }
+        public int Salary { get; set; }
+
+    }
+}
---
+++ Sample-Dapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Sample_Dapper;
+using System.Data.SqlClient;
+
+
+namespace Sample_Dapper
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string connectionString = "Server=DESKTOP-SFJ3E08\\SQLEXPRESS;Database=Speehive;Integrated Security=True;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            var emp = new Employees
+            {
+                EmpName = "Geo",
+                City = "Thrissur",
+                Salary = 10
+            };
+
+            string insertQuery = "INSERT INTO Employee (EmpName,City,Salary) VALUES (@EmpName, @City, @Salary);";
+            conn.Execute(insertQuery, emp);
+
+        }
+    }
+}
