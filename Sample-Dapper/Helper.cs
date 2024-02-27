8ec6f2f1d6d7718919c5084d51897023986bea08
Cindila Sunny <cindilakaranchirakkal@gmail.com>
2024-02-27 21:55:03 +0530

    Added Helper class

---
+++ Sample-Dapper/Helper.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+namespace Sample_Dapper
+{
+    public class Helper
+    {
+        public void Insertion(SqlConnection conn, Employees emp)
+        {
+            string insertQuery = "INSERT INTO Employee (EmpName,City,Salary) VALUES (@EmpName, @City, @Salary);";
+            conn.Execute(insertQuery, emp);
+        }
+
+        public void Deletion(SqlConnection conn, string empName)
+        {
+            string deleteQuery = "DELETE FROM Employee WHERE EmpName = @EmpName;";
+            conn.Execute(deleteQuery, new { EmpName = empName });
+        }
+        public Employees Retrival(SqlConnection conn, string empName)
+        {
+            string retrieveQuery = "SELECT * FROM Employee WHERE EmpName = @EmpName;";
+            return conn.QueryFirstOrDefault<Employees>(retrieveQuery, new { EmpName = empName });
+        }
+        public int Updation(SqlConnection conn, string empName, decimal newSalary)
+            {
+            string updateQuery = "UPDATE Employee SET Salary = @Salary WHERE EmpName = @EmpName;";
+            return conn.Execute(updateQuery, new { Salary = newSalary, EmpName = empName });
+        }
+    }
+}
+
--- Sample-Dapper/Program.cs
+++ Sample-Dapper/Program.cs
@@ -1,55 +1,54 @@
 ﻿using Dapper;
 using Sample_Dapper;
 using System.Data.SqlClient;
-
-
 namespace Sample_Dapper
 
 
 {
-    internal class Program
+ public class Program
     {
         static void Main(string[] args)
         {
-           string connectionString = "Server=DESKTOP-SFJ3E08\\SQLEXPRESS;Database=Speehive;Integrated Security=True;";
-            SqlConnection conn = new SqlConnection(connectionString);
-
-            var emp = new Employees
+            string connectionString = "Server=DESKTOP-SFJ3E08\\SQLEXPRESS;Database=Speehive;Integrated Security=True;";
+            using (var conn = new SqlConnection(connectionString))
             {
-                EmpName = "Geo",
-                City = "Thrissur",
-                Salary = 10
-            };
+                var helper = new Helper();
+                conn.Open();
+                var emp = new Employees
+                {
+                    EmpName = "Geo",
+                    City = "Thrissur",
+                    Salary = 10
+                };
 
-            string insertQuery = "INSERT INTO Employee (EmpName,City,Salary) VALUES (@EmpName, @City, @Salary);";
-            conn.Execute(insertQuery, emp);
+                helper.Insertion(conn, emp);
 
-            // Delete employee "Geo"
-            string deleteQuery = "DELETE FROM Employee WHERE EmpName = @EmpName;";
-            conn.Execute(deleteQuery, new { EmpName = "Geo" });
 
-            // Retrieving employee 
-            string retrieveQuery = "SELECT * FROM Employee WHERE EmpName = @EmpName;";
-            var retrievedEmployee = conn.QueryFirstOrDefault<Employees>(retrieveQuery, new { EmpName = "mridual" });
+                // Delete employee "Geo"
+                helper.Deletion(conn, "Geo");
+                // Retrieving employee 
 
-            // Printing retrieved employee details
-            if (retrievedEmployee != null)
-            {
-                Console.WriteLine($"Retrieved Employee: {retrievedEmployee.EmpName}, City: {retrievedEmployee.City}," +
-                    $" Salary: {retrievedEmployee.Salary}");
+                var retrievedEmployee = helper.Retrival(conn, "mridual");
 
-                // Updating the salary of employee 
-                string updateQuery = "UPDATE Employee SET Salary = @Salary WHERE EmpName = @EmpName;";
-                int rowsAffected = conn.Execute(updateQuery, new { Salary = 10000, EmpName = "mridual" });
+                // Printing retrieved employee details
+                if (retrievedEmployee != null)
+                {
+                    Console.WriteLine($"Retrieved Employee: {retrievedEmployee.EmpName}, City: {retrievedEmployee.City}," +
+                        $" Salary: {retrievedEmployee.Salary}");
 
-                Console.WriteLine($"Rows updated: {rowsAffected}");
+                    // Updating the salary of employee 
+
+                    int rowsAffected = helper.Updation(conn, "mridual", 1000);
+
+                    Console.WriteLine($"Rows updated: {rowsAffected}");
+                }
+                else
+                {
+                    Console.WriteLine("Employee not found.");
+                }
+
+
             }
-            else
-            {
-                Console.WriteLine("Employee not found.");
-            }
-
-
         }
     }
 }
